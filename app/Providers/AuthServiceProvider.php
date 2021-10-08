4809357ed5b27d5d45f9304eca717cf6ee7e2c29
David Spak <david.spak478@gmail.com>
2021-10-08 21:53:03 +0200

    Auth user query, fix trait import

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,6 +31,6 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::tokensExpireIn(Carbon::now()->addMinutes(10));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));
-        Passport::personalAccessTokensExpireIn(Carbon::now()->addDays(50));
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addDays(1));
     }
 }
