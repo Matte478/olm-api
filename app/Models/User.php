39671e4eda423645edd79cb19da650e3f6c5af2c
David Spak <david.spak478@gmail.com>
2021-10-31 14:54:54 +0100

    Spatie - laravel permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, hasRoles;
 
     /**
      * The attributes that are mass assignable.
