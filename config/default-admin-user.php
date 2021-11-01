0004e2879845a541b910ee2409f4cf0a2aa09d9a
David Spak <david.spak478@gmail.com>
2021-11-01 10:41:33 +0100

    Fill default user, roles & permissions

---
+++ config/default-admin-user.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'email' => env('DEFAULT_ADMIN_USER_EMAIL', null),
+    'name' => env('DEFAULT_ADMIN_USER_NAME', null),
+    'password' => env('DEFAULT_ADMIN_USER_PASSWORD', null),
+];
---
+++ database/migrations/2021_10_31_140233_fill_default_user_and_permissions.php
@@ -0,0 +1,175 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class FillDefaultUserAndPermissions extends Migration
+{
+    protected $roles;
+    protected $permissions;
+    protected $guardName = 'api';
+    protected $users;
+    protected $email;
+    protected $name;
+    protected $password;
+
+    /**
+     * FillDefaultUserAndPermissions constructor.
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->email = config('default-admin-user.email');
+        $this->name = config('default-admin-user.name');
+        $this->password = config('default-admin-user.password');
+
+        if(!$this->email || !$this->name || !$this->password) {
+            throw new Exception('Invalid default admin user credentials.');
+        }
+
+        $adminPermissions = collect([
+            // users
+            'user.index',
+            'user.show',
+            'user.edit',
+            'user.delete',
+
+            // roles
+            'role.index',
+            'role.create',
+            'role.show',
+            'role.edit',
+            'role.delete',
+        ]);
+
+        $studentPermissions = collect([
+           //
+        ]);
+
+        $allPermissions = $adminPermissions->merge($studentPermissions)->unique();
+
+        $this->permissions = $allPermissions->map(function($permission) {
+            return [
+                'name' => $permission,
+                'guard_name' => $this->guardName,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ];
+        })->toArray();
+
+        $this->roles = [
+            [
+                'name' => 'Administrator',
+                'guard_name' => $this->guardName,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'permissions' => $adminPermissions,
+            ],
+            [
+                'name' => 'Student',
+                'guard_name' => $this->guardName,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'permissions' => $studentPermissions,
+            ],
+        ];
+
+        $this->users = [
+            [
+                'name' => 'Administrator',
+                'email' => $this->email,
+                'password' => Hash::make($this->password),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'roles' => [
+                    'Administrator'
+                ],
+                'permissions' => [
+                    //
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function () {
+            foreach ($this->permissions as $permission) {
+                DB::table('permissions')->insert($permission);
+            }
+
+            foreach ($this->roles as $role) {
+                $permissions = $role['permissions'];
+                unset($role['permissions']);
+
+                $roleId = DB::table('roles')->insertGetId($role);
+
+                $permissionItems = DB::table('permissions')->whereIn('name', $permissions)->get();
+                foreach ($permissionItems as $permissionItem) {
+                    DB::table('role_has_permissions')->insert(['permission_id' => $permissionItem->id, 'role_id' => $roleId]);
+                }
+            }
+
+            foreach ($this->users as $user) {
+                $roles = $user['roles'];
+                unset($user['roles']);
+                $permissions = $user['permissions'];
+                unset($user['permissions']);
+
+                $userId = DB::table('users')->insertGetId($user);
+
+                $roleItems = DB::table('roles')->whereIn('name', $roles)->get();
+                foreach ($roleItems as $roleItem) {
+                    DB::table('model_has_roles')->insert(['role_id' => $roleItem->id, 'model_id' => $userId, 'model_type' => 'App\Models\User']);
+                }
+
+                $permissionItems = DB::table('permissions')->whereIn('name', $permissions)->get();
+                foreach ($permissionItems as $permissionItem) {
+                    DB::table('model_has_permissions')->insert(['permission_id' => $permissionItem->id, 'model_id' => $userId, 'model_type' => 'App\Models\User']);
+                }
+            }
+        });
+        app()['cache']->forget(config('permission.cache.key'));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::transaction(function () {
+            foreach ($this->users as $user) {
+                if(!empty($userItem = DB::table('users')->where('email', '=', $user['email'])->first())) {
+                    DB::table('users')->where('id', '=', $userItem->id)->delete();
+                    DB::table('model_has_permissions')->where('model_id', '=', $userItem->id)->where('model_type', '=', 'App\Models\User')->delete();
+                    DB::table('model_has_roles')->where('model_id', '=', $userItem->id)->where('model_type', '=', 'App\Models\User')->delete();
+                }
+            }
+
+            foreach ($this->roles as $role) {
+                if(!empty($roleItem = DB::table('roles')->where('name', '=', $role['name'])->first())) {
+                    DB::table('roles')->where('id', '=', $roleItem->id)->delete();
+                    DB::table('model_has_roles')->where('role_id', '=', $roleItem->id)->delete();
+                }
+            }
+
+            foreach ($this->permissions as $permission) {
+                if(!empty($permissionItem = DB::table('permissions')->where('name', '=', $permission['name'])->first())) {
+                    DB::table('permissions')->where('id', '=', $permissionItem->id)->delete();
+                    DB::table('model_has_permissions')->where('permission_id', '=', $permissionItem->id)->delete();
+                }
+            }
+        });
+        app()['cache']->forget(config('permission.cache.key'));
+    }
+}
