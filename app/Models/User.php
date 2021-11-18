8bd2ce55f3d3a08913869d5636d81da9ea3f7797
David Spak <david.spak478@gmail.com>
2021-11-18 22:41:01 +0100

    Assign student role after registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,9 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, hasRoles;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasRoles;
+
+    protected $guard_name = 'api';
 
     /**
      * The attributes that are mass assignable.
